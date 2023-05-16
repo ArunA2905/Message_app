3658588e21ac0c3d3cd13d9773954b16dc2f04a3
ArunA2905 <viswa6919@gmail.com>
2023-05-16 14:52:16 +0530

    Upto CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_145537) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_12_111931) do
   create_table "messages", force: :cascade do |t|
     t.text "message"
     t.datetime "created_at"
