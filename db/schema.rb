6399a1d5c3058a55e05272a9c210d119a3d6fe67
ArunA2905 <viswa6919@gmail.com>
2023-05-22 13:16:36 +0530

    added-address-pincode-ph_no

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_134628) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_071625) do
   create_table "messages", force: :cascade do |t|
     t.text "message"
     t.datetime "created_at"
@@ -27,6 +27,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_16_134628) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "address"
+    t.string "pincode"
+    t.string "phonenumber"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
